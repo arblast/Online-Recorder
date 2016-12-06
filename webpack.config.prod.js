efdb1f73b39fc5307cbc7de1c52bceac0813c94c
Justice Yen <justice.yen@gmail.com>
2016-12-06 10:13:07 -0800

    add production webpack config

---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: './frontend/record_cloud.jsx',
+  output: {
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  devtool: 'cheap-module-source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx' ]
+  }
+};
