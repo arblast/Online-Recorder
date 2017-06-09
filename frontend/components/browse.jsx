0b90418fdf02b592f804d3fb3eb0f381e71bc58d
Justice Yen <justice.yen@gmail.com>
2017-06-08 18:08:50 -0700

    fix issue with overflow

--- frontend/components/browse.jsx
+++ frontend/components/browse.jsx
@@ -69,25 +69,27 @@ class Browse extends React.Component {
               return (
                 <div className="slider-container" key={category}>
                   <h2 className="top-recording-title">Top Recordings for {category}</h2>
-                  <div className="browse-slider">
-                    {arrowLeft}
-                    {
-                      this.props.recordings[category].map((recording, idx) => {
-                        return (
-                          <div key={idx} className="slider-item" style={this[`${category}Style`]} onClick={this.recordingDetail(recording.id)}>
-                            <div className="thumbnail-container">
-                              <div className="thumbnail" style={{backgroundImage: 'url(' + recording.image_url+ ')'}}></div>
+                  <div className="browse-slider-wrapper">
+                    <div className="browse-slider">
+                      {arrowLeft}
+                      {
+                        this.props.recordings[category].map((recording, idx) => {
+                          return (
+                            <div key={idx} className="slider-item" style={this[`${category}Style`]} onClick={this.recordingDetail(recording.id)}>
+                              <div className="thumbnail-container">
+                                <div className="thumbnail" style={{backgroundImage: 'url(' + recording.image_url+ ')'}}></div>
+                              </div>
+                              <div className="recording-title-container">
+                                <div className="recording-title">{recording.title}</div>
+                                <div className="more-description" onClick={this.moreDescription}><div className="description-arrow left-arrow"></div><div className="description-arrow right-arrow"></div></div>
+                                <div className="recording-description">{recording.description}</div>
+                              </div>
                             </div>
-                            <div className="recording-title-container">
-                              <div className="recording-title">{recording.title}</div>
-                              <div className="more-description" onClick={this.moreDescription}><div className="description-arrow left-arrow"></div><div className="description-arrow right-arrow"></div></div>
-                              <div className="recording-description">{recording.description}</div>
-                            </div>
-                          </div>
-                        );
-                      })
-                    }
-                    {arrowRight}
+                          );
+                        })
+                      }
+                      {arrowRight}
+                    </div>
                   </div>
                 </div>
               );
