555b52b8b7b359ac64a97b8f9d4832a14104ca1d
Justice Yen <justice.yen@gmail.com>
2018-10-11 19:01:58 -0700

    add user mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'do-not-reply@record-cloud.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url = 'https://record-cloud.com'
+    mail(to: @user.email, subject: 'Welcome to Record Cloud')
+  end
+end
